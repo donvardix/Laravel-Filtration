e0e463063cf360b8ce48594da4e09cd53214feef
LaRi <donvardix@gmail.com>
2020-03-14 16:59:03 +0200

    create user seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,10 +19,8 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
+        'nickname' => $faker->unique()->userName,
+        'account_id' => $faker->unique()->randomNumber($nbDigits = 7),
+        'global_rating' => $faker->unique()->numberBetween($min = 1000, $max = 12000),
     ];
 });
